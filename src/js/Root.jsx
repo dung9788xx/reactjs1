705a19b2311354b934ec6196d634f9ad4a896063
dungtv <dungtv@hblab.vn>
2020-08-18 15:21:04 +0700

    abc

--- src/js/Root.jsx
+++ src/js/Root.jsx
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import Login from "./components/login/login";
 import Register from "./components/register/register";
-import Home from "./components/home/home";
+import Home from "./components/home/home.jsx";
+import Product from "./components/products/product";
 import  {BrowserRouter as Router,Route} from 'react-router-dom'
 class Root extends Component{
     render() {
@@ -10,6 +11,7 @@ class Root extends Component{
                 <Route exact path='/' component={Home}/>
                 <Route path='/register' component={Register}/>
                 <Route path='/login' component={Login}/>
+                <Route path='/quan-ao-nam' component={Product}/>
             </Router>
         )
     }
