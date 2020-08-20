6eca321d2427569a635a1ec35b6bf6b0d4d293d8
dungtv <dungtv@hblab.vn>
2020-08-20 16:56:50 +0700

    'abc'

--- src/js/components/home/home.jsx
+++ src/js/components/home/home.jsx
@@ -5,6 +5,8 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 import Register from "../register/register";
 import Product from "../products/product";
 import {API} from '../constConfig/apiConfig'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from "react-bootstrap/Card";
 import axios from 'axios';
 
 axios.defaults.headers.common = {'Authorization': `Bearer ${localStorage.getItem('access_token')}`}
@@ -15,6 +17,7 @@ class Home extends React.Component {
         this.state = ({
             productCategories: null,
             activeKey:0,
+            toggleStatus:0,
             isLoading:true
         });
     }
@@ -39,6 +42,10 @@ class Home extends React.Component {
        const handleSelect = (eventKey) =>{
            this.setState({activeKey:eventKey});
        }
+       const handleSelectToggle = (eventKey) =>{
+            this.setState({toggleStatus:this.state.toggleStatus ==1 ? 0:1});
+        }
+
        let menu=null;
        if(this.state.activeKey==0){
            menu=" Trang chu"
@@ -46,18 +53,60 @@ class Home extends React.Component {
 
            menu=<Product productCategory={this.state.activeKey}/>
        }
-       let productCategoryMenu= this.state.productCategories ? this.state.productCategories.map((category,i)=>{
-           return (
-               <Nav.Item>
-                   <Nav.Link eventKey={category.id}>
-                       {category.name}
-                   </Nav.Link>
-               </Nav.Item>
-           )
-       }) : null;
+       let show=(this.state.toggleStatus == 1) ? 'visible':'invisible h-0';
+        let productCategoryMenu=null;
+        let dropdownProductCategory=null;
+        if(this.state.productCategories) {
+           productCategoryMenu = this.state.productCategories.map((category, i) => {
+               if(i<5){
+                   return (
+                       <Nav.Item>
+                           <Nav.Link eventKey={category.id}>
+                               {category.name}
+                           </Nav.Link>
+                       </Nav.Item>
+                   )
+               }
+           });
+           if(this.state.productCategories.length >=5){
+               dropdownProductCategory= this.state.productCategories.map((category, i) => {
+                   if(i>=5){
+                       return (
+                           <Nav.Item>
+                               <Nav.Link eventKey={category.id}>
+                                   {category.name}
+                               </Nav.Link>
+                           </Nav.Item>
+                       )
+                   }
+               });
+           }
+
+       }
+        let dropdownIcon=null;
+        if(this.state.toggleStatus==1){
+
+            dropdownIcon= <svg width="1em" height="1em" viewBox="0 0 16 16"
+                               className="bi bi-chevron-double-up" fill="currentColor"
+                               xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"/>
+                <path fill-rule="evenodd"
+                      d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+            </svg>
+        }else{
+            dropdownIcon=<svg width="1em" height="1em" viewBox="0 0 16 16"
+                              className="bi bi-chevron-double-down" fill="currentColor"
+                              xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                <path fill-rule="evenodd"
+                      d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        }
         const style = {
             height: '100vh',
-            width:'100vw'
+            width:'98vw'
         }
         if(this.state.isLoading){
             return (
@@ -71,14 +120,26 @@ class Home extends React.Component {
         }else
         return (
             <div style={style} className='container-fluid  '>
-                   <Nav className='bg-light sticky-top p-2 rounded' onSelect={handleSelect} variant="pills" activeKey={this.state.activeKey} >
-                      <div style={{font:menu}} className='row col-8'>
+                   <Nav className='bg-light sticky-top p-2 rounded pl-5' onSelect={handleSelect} variant="pills" activeKey={this.state.activeKey} >
+                      <div style={{font:menu}} className='row col-12 col-sm-8'>
                           <Nav.Item>
                               <Nav.Link eventKey='0'>
                                   Trang chủ
                               </Nav.Link>
                           </Nav.Item>
                           {productCategoryMenu}
+                          <Nav.Item>
+                                  <div className='mt-1' onClick={handleSelectToggle}>
+                                      {dropdownIcon}
+                                  </div>
+
+                          </Nav.Item>
+
+                          <div style={{height: this.state.toggleStatus==0 ? '0px':''}} className={'row col-12 '+show}>
+                              <div className='row'>
+                                  {dropdownProductCategory}
+                              </div>
+                          </div>
                           {/*<NavDropdown title="Dropdown" id="nav-dropdown">*/}
                           {/*    <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>*/}
                           {/*    <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>*/}
@@ -87,39 +148,18 @@ class Home extends React.Component {
                           {/*    <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>*/}
                           {/*</NavDropdown>*/}
                       </div>
-                      <div className='row col-4 justify-content-end '>
+                      <div className='row col-12  col-sm-4 justify-content-end '>
                               <form className="form-inline d-flex justify-content-end">
                                   <input className="form-control mr-sm-2" type="search" placeholder="Search"
                                          aria-label="Search"/>
                                   <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                               </form>
                       </div>
+
                    </Nav>
-                {menu}
-                {/*<div className='row'>*/}
-                {/*    <div className='col-12 col-sm-6 col-lg-4 bg-info'>*/}
-                {/*        <p className="lead">*/}
-                {/*            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non*/}
-                {/*            commodo luctus.*/}
-                {/*        </p>*/}
-                {/*    </div>*/}
-                {/*    <div className='col-12 col-sm-6 col-lg-4  bg-danger'>*/}
-                {/*        <p>You can use the mark tag to <mark>highlight</mark> text.</p>*/}
-                {/*        <p><del>This line of text is meant to be treated as deleted text.</del></p>*/}
-                {/*        <p><s>This line of text is meant to be treated as no longer accurate.</s></p>*/}
-                {/*        <p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>*/}
-                {/*        <p><u>This line of text will render as underlined</u></p>*/}
-                {/*        <p><small>This line of text is meant to be treated as fine print.</small></p>*/}
-                {/*        <p><strong>This line rendered as bold text.</strong></p>*/}
-                {/*        <p><em>This line rendered as italicized text.</em></p>*/}
-                {/*    </div>*/}
-                {/*    <div className='col-12 col-sm-6 col-lg-4  bg-warning'>*/}
-                {/*        c*/}
-                {/*    </div>*/}
-                {/*    <div className='col-12 col-sm-6 col-lg-4  bg-danger'>*/}
-                {/*        d*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+
+                  {menu}
+
             </div>
         )
     }
